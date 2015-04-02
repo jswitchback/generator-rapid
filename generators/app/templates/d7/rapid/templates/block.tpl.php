62e0607cf5433b821cf331d1cca9d74112cd6e2b
Joe Maag <jmaag@rapiddg.com>
2015-04-02 14:38:52 -0400

    block template adjustment & p tag margin reset

--- generators/app/templates/d7/rapid/templates/block.tpl.php
+++ generators/app/templates/d7/rapid/templates/block.tpl.php
@@ -14,7 +14,7 @@
     <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
-
-  <?php print $content; ?>
-
+  <div class="block-content">
+    <?php print $content; ?>
+  </div>
 </div>
